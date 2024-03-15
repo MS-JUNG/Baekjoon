f3014fd74f96690c9fe4723a5e313d236e2b60d2
minsu <lukases1501@gmail.com>
2024-03-15 09:51:04 +0900

    'solve'

---
+++ 2563.py
@@ -0,0 +1,49 @@
+count = int(input())
+
+paper = [[1 for _ in range(100)] for _ in range(100)]
+
+print(paper)
+for i in range(count):
+    a, b = map(int, input().split())
+    for j in range(a,a+10):
+        
+        for k in range(b, b+10):
+            if j>=100 | k>=100:
+                break
+    
+            paper[j][k] = 1
+total = 0
+
+for i in paper:
+    total += sum(i)
+
+print(total)
+
+
+
+## 틀린 방법  ->> 교집합을 여러번 빼서 중복햇 뺴는 문제 발생 
+
+# count = int(input())
+# square = []
+# for i in range(count):
+#     a, b = map(int, input().split())
+#     square.append([a, b])
+
+# first = 100 * count
+# for i in range(count):
+#     sq = square.pop()
+
+#     for i in range(len(square)):
+
+#         if ((abs(sq[0] - square[i][0]) >= 10) or (abs(sq[1] - square[i][1]) >= 10)):
+#             pass
+#         else: 
+
+#             cross = (10-abs(sq[0]-square[i][0]))*(10-abs(sq[1]-square[i][1]))
+#             first -= cross
+
+         
+    
+# print(first)
+
+
